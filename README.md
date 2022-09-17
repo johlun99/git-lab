2ed5d67ff53a032628d074dcf75c336b555d619a
Johan Lundgren <lundgren.johan99@gmail.com>
2022-09-17 15:49:36 +0200

    add(README.md): Added wonderful disclaimer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # git-lab
 just for messing around with git and testing what works so I don't screw up anything in production for anyone else :P 
+
+Who am I kidding, nobody ever fucks up production o.O
--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 print("Hello world!")
 
+# This adds 2 values
 def adder(x, y):
     return x + y
 
-
+# This subtracts x from y
 def subtractor(x, y):
     return x - y
 
+# This multiplies 2 values
 def multiplier(x, y):
     return x * y
